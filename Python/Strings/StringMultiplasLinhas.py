b53dd99f737ce0f73d24abd1b25653f36a6c471d
Cyro Rocha <gabsmr2211@gmail.com>
2025-01-11 19:24:31 -0300

    ADD strings

---
+++ Python/Strings/StringMultiplasLinhas.py
@@ -0,0 +1,19 @@
+nome = "Cyro"
+
+mensagem = """
+    Olá meu nome é {nome},
+eu estou aprendendo python. """
+
+print(mensagem)
+
+
+print(""" 
+      ***************Menu***************
+      
+    1 - Depositar
+    2 - Sacar
+    3 - Saldo
+    # - Sair
+    *************************************
+      
+        Obrigado por usar nosso sistema""")
