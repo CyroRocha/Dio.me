31acce5736c4fdc4c583e6f5a5e55785ca0948c0
Cyro Rocha <gabsmr2211@gmail.com>
2025-03-21 19:42:44 -0300

    add conjuntos

---
+++ Python/01-Estrutura- de-dados/03 - Conjuntos/05_diference.py
@@ -0,0 +1,8 @@
+conjunto_a = {1,2,3}
+conjunto_b = {2,3,5}
+
+conjuntoadiff = conjunto_a.difference(conjunto_b) # {1}
+conjuntobdiff = conjunto_b.difference(conjunto_a) # {5}
+
+print(conjuntoadiff)
+print(conjuntobdiff)
---
+++ Python/01-Estrutura- de-dados/03 - Conjuntos/11_clear.py
@@ -0,0 +1,5 @@
+sorteio = {1, 23, 25, 42}
+
+print(sorteio)
+sorteio.clear()
+print(sorteio)
