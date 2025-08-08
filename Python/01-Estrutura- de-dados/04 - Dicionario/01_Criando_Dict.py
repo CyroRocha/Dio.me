21e0a71d6be6ea1d13124eefc61e6bf18dc4c865
Cyro Rocha <gabsmr2211@gmail.com>
2025-08-08 15:37:13 -0300

    change criando dict para 01 criando dict e add 02 dicionario aninhado

---
+++ Python/01-Estrutura- de-dados/04 - Dicionario/01_Criando_Dict.py
@@ -0,0 +1,21 @@
+pessoa1 = {"Nome": "Guilherme", "Idade": 28}
+pessoa2 = dict(Nome="Guilherme", Idade= 28)
+
+print(pessoa1)
+print(pessoa2)
+
+pessoa1["Telefone"] = "3333-1234"
+
+print(pessoa1)
+print(pessoa2)
+
+pessoa1["Nome"]
+pessoa1["Idade"]
+pessoa1 ["telefone"]
+
+pessoa2["Nome"] = "Maria"
+pessoa2["Idade"] = 18
+pessoa2["Telefone"] = "99988-1781" 
+
+print(pessoa1)
+print(pessoa2)
---
+++ Python/01-Estrutura- de-dados/04 - Dicionario/02_Dicionario_aninhado.py
@@ -0,0 +1,9 @@
+contatos = {
+    "guilherme@gmail.com": {"Nome": "Guilherme", "Telefone": "3333-2222"},
+    "giovanna@gmail.com": {"Nome": "Giovanna", "Telefone": "3443-2121"},
+    "chappie@gmail.com": {"Nome":"Chappie", "Telefone":"3344-9871"},
+    "melaine@gmail.com": {"Nome": "Melaine", "Telefone": "3333-7766"}
+}
+
+Dados = contatos["giovanna@gmail.com"]["Telefone"]
+print(Dados)
